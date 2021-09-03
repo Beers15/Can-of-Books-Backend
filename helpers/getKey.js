8da7d80adc51daaaab89c2eaa1a0fe9ecd93de27
Beers15 <alexanderbeers15@gmail.com>
2021-09-03 09:29:43 -0700

    Change JWTClient domain name

--- helpers/getKey.js
+++ helpers/getKey.js
@@ -3,7 +3,7 @@
 const jwksClient = require('jwks-rsa');
 // const domain = 'dev-y5ehm8nw.us.auth0.com';
 const client = jwksClient({
-  jwksUri: 'https://dev-4rwdhvtd.us.auth0.com/.well-known/jwks.json',
+  jwksUri: 'https://dev-1n4otpy6.us.auth0.com/.well-known/jwks.json',
 });
 
 module.exports = (header, callback) => {
